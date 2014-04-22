40e54e24f22b1f35618d4dd1e97ccddb11618fb0
JasonFromMar <wsy717846100@qq.com>
2014-04-22 16:44:51 +0800

    ScoketTest
    Test From Internet

---
+++ ChatClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Collections;
+
+namespace Program
+{
+    class ChatClient
+    {
+        public static Hashtable ALLClients = new Hashtable();//客户列表
+        private TcpClient _client;//客户端实体
+        public string _clientIP;//客户端IP
+        private string _clientNick;//客户端昵称
+
+        private byte[] data;
+        private bool ReceiveNick = true;
+
+        public ChatClient(TcpClient client)
+        {
+            this._client = client;
+            this._clientIP = client.Client.RemoteEndPoint.ToString();
+
+            //把当前客户端实例添加到客户列表当中
+            ALLClients.Add(this._clientIP,this);
+            data =new byte[this._client.ReceiveBufferSize];
+            //服务端获取消息
+            client.GetStream().BeginRead(data, 0, System.Convert.ToInt32(this._client.ReceiveBufferSize), ReceiveMessage, null);
+        }
+        public void ReceiveMessage(IAsyncResult ar)
+        {
+            int bytesRead;
+            try
+            {
+                lock (this._client.GetStream())
+                {
+                    bytesRead = this._client.GetStream().EndRead(ar);
+                }
+                if (bytesRead < 1)
+                {
+                    ALLClients.Remove(this._clientIP);
+                    Broadcast(this._clientNick + "has left the that");
+
+                    return;
+                }
+                else
+                {
+                    string messageReceived = System.Text.Encoding.ASCII.GetString(data, 0, bytesRead);
+                    if (ReceiveNick)
+                    {
+                        this._clientNick = messageReceived;
+                        Broadcast(this._clientNick+"has joined the chat.");
+
+                        //this.sendMessage("hello");
+                        ReceiveNick=false;
+                    }
+                    else
+                    {
+                        Broadcast(this._clientNick+">"+messageReceived);
+                    }
+                }
+                lock(this._client.GetStream())
+                {
+                    this._client.GetStream().BeginRead(data,0,System.Convert.ToInt32(this._client.ReceiveBufferSize), ReceiveMessage, null);
+                }
+            }
+            catch(Exception ex)
+            {
+                ALLClients.Remove(this._clientIP);
+
+                Broadcast(this._clientNick+"has left the chat.");
+            }
+        }
+        public void seedMessage(string message)
+        {
+            try
+            {
+                System.Net.Sockets.NetworkStream ns;
+                lock (this._client.GetStream())
+                {
+                    ns = this._client.GetStream();
+                }
+                //对消息进行编码
+                byte[] bytesToSend = System.Text.Encoding.ASCII.GetBytes(message);
+                ns.Write(bytesToSend, 0, bytesToSend.Length);
+                ns.Flush();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        //向客户端广播消息
+        public void Broadcast(string message)
+        {
+            Console.WriteLine(message);
+            foreach (DictionaryEntry c in ALLClients)
+            {
+                ((ChatClient)(c.Value)).seedMessage(message+Environment.NewLine);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Program
+{
+    class Program
+    {
+        // 设置连接端口        
+        const int portNo = 500;
+        static void Main(string[] args)
+        {
+            // 初始化服务器IP            
+            System.Net.IPAddress localAdd = System.Net.IPAddress.Parse("127.0.0.1");            
+            // 创建TCP侦听器            
+            TcpListener listener = new TcpListener(localAdd, portNo);            
+            listener.Start();            // 显示服务器启动信息            
+            Console.WriteLine("Server is starting...\n");            
+            // 循环接受客户端的连接请求            
+            while (true)            
+            {
+                ChatClient user = new ChatClient(listener.AcceptTcpClient());
+                // 显示连接客户端的IP与端口                
+                Console.WriteLine(user._clientIP + " is joined...\n");            
+            }   
+        }
+    }
+}
